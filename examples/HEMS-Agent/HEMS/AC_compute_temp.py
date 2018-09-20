bb0c1700862bf9f20a20f3173ee4ab5e8b96661b
raj <cyrus19901@gmail.com>
2018-09-20 15:32:56 -0700

    removing the un-necessary files

--- examples/HEMS-Agent/HEMS/AC_compute_temp.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def AC_compute_temp(A,B,C,T_out,Q_i,Q_s,Q_h,T_a,mdt,ddt):
-
-    temp = T_a;
-    for i in np.arange(0,int(mdt/ddt)):
-        temp = A*temp + B*T_out + C*Q_h + C*(0.5*Q_i+0.5*Q_s)
-    return temp
